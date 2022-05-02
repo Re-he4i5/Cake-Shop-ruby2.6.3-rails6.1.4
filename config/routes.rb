ddcd03926dbf3fb1cc9edc879b8b3ae32d968fe9
Re-he4i5 <kuronekoryohei@gmail.com>
2022-05-03 00:22:34 +0900

    [add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+  }
+
+  devise_for :customer, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
+  }
+
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+
+  end
+
+
+  scope module: :public do
+    root 'homes#top'
+
+    get 'customer/mypage' => 'customers#show', as: 'mypage'
+
+  end
+
 end
