f8be1579e3e817d885540b905d16f35030f91420
Re-he4i5 <kuronekoryohei@gmail.com>
2022-05-03 22:02:06 +0900

    [add]2_feature_account_manage

---
+++ app/controllers/public/registrations_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  def after_sign_up_path_for(resource)
+    mypage_path
+  end
+
+  def after_update_path_for(resource)
+    mypage_path
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+  }
+
+  devise_for :customer, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
+  }
+
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+
+    resources :customers, only: [:index, :show, :edit, :update]
+
+  end
+
+
+  scope module: :public do
+    root 'homes#top'
+
+    get 'customer/mypage' => 'customers#show', as: 'mypage'
+    get 'customer/information/edit' => 'customers#edit', as: 'edit_information'
+    patch 'customer/information' => 'customers#update', as: 'update_information'
+
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
+
+  end
+
 end
