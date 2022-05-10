9cfa21aa7c37193327c4d8759902f2db31c13df8
Re-he4i5 <kuronekoryohei@gmail.com>
2022-05-10 23:08:36 +0900

    init

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-
-/*Bootstrap導入*/
-
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: 'popper.js'
-  })
-)
